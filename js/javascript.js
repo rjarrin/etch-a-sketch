be4083ba92f504a38a1accb5182bdf1a991253f4
rjarrin <jarrin.ricardo@gmail.com>
2024-02-29 15:00:36 -0500

    Fixed grid overflow + added title header

--- js/javascript.js
+++ js/javascript.js
@@ -66,24 +66,7 @@ function generateGrid(size) {
 // Identify the grid's container element
 let container = document.querySelector(".container");
 
-// Reference grid dimensions (to be changed for UI later)
+// Reference grid dimensions
 let gridDim = 16;
 
-// Create grid according to grid dimensions
-for (let i = 0; i < gridDim; i++) {
-    // Prepare row
-    const row = document.createElement('div');
-    row.classList.add("grid-row");
-    // Iterate through row's columns
-    for (let j = 0; j < gridDim; j++) {
-        // Prepare square
-        const square = document.createElement('div');
-        square.classList.add("grid-square");
-        // Change-color-on-hover functionality
-        square.addEventListener("mouseover", function() {
-            square.style.backgroundColor = getRandomColor();
-        });
-        row.append(square);
-    }
-    container.appendChild(row);
-}
+generateGrid(gridDim);
